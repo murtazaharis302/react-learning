b97d45a7e441f4949c760ccd15f8aeb263ec9660
murtazaharis302 <murtazaharis302@gmail.com>
2025-12-10 17:55:16 +0500

    fixed lock issue

--- my-app/src/contexts/AppContext.jsx
+++
@@ -1,9 +0,0 @@
-
-import { createContext } from "react";
-
-export const AppContext = createContext({
-  unit: "C", // "C" or "F"
-  refreshTime: 10000, // milliseconds (default 10s)
-  setUnit: () => {},
-  setRefreshTime: () => {},
-});
